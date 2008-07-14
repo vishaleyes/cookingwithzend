324bdb34d7a94b4288a688e9ae21153a40af29a0
cookery@catharsis.co.uk <cookery@catharsis.co.uk@64c92516-bd51-0410-8049-8bbf4b82af0d>
2008-07-14 01:12:16 +0000

    Minor schema change

--- trunk/docs/schema.sql
+++ trunk/docs/schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.11
+-- MySQL dump 10.10
 --
 -- Host: localhost    Database: recipe_development
 -- ------------------------------------------------------
--- Server version	5.0.51a
+-- Server version	5.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,8 +20,6 @@
 --
 
 DROP TABLE IF EXISTS `comments`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL auto_increment,
   `user_id` int(11) default NULL,
@@ -29,151 +27,89 @@ CREATE TABLE `comments` (
   `recipe_id` int(11) default NULL,
   `created` datetime default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
-
---
--- Dumping data for table `comments`
---
-
-LOCK TABLES `comments` WRITE;
-/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
-UNLOCK TABLES;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `ingredient_measurements`
 --
 
 DROP TABLE IF EXISTS `ingredient_measurements`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `ingredient_measurements` (
   `id` int(11) NOT NULL auto_increment,
   `ingredient_id` int(11) default NULL,
   `measurement_id` int(11) default NULL,
   `position` int(11) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
-
---
--- Dumping data for table `ingredient_measurements`
---
-
-LOCK TABLES `ingredient_measurements` WRITE;
-/*!40000 ALTER TABLE `ingredient_measurements` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ingredient_measurements` ENABLE KEYS */;
-UNLOCK TABLES;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `ingredients`
 --
 
 DROP TABLE IF EXISTS `ingredients`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `ingredients` (
   `id` int(11) NOT NULL auto_increment,
   `name` text NOT NULL,
-  `amount` float default NULL,
-  `recipe_id` int(11) default NULL,
-  `quantity` int(9) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
-
---
--- Dumping data for table `ingredients`
---
-
-LOCK TABLES `ingredients` WRITE;
-/*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
-UNLOCK TABLES;
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `name` (`name`(255))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `measurements`
 --
 
 DROP TABLE IF EXISTS `measurements`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `measurements` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(255) default NULL,
   `abbreviation` varchar(255) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
-
---
--- Dumping data for table `measurements`
---
-
-LOCK TABLES `measurements` WRITE;
-/*!40000 ALTER TABLE `measurements` DISABLE KEYS */;
-INSERT INTO `measurements` VALUES (1,'Tablespoon','tbsp'),(2,'Teaspoon','tsp'),(3,'Grams','g'),(4,'Kilograms','kg'),(5,'Litre','l'),(6,'Millilitre','ml'),(7,'Centimetre','cm'),(8,'Quarter','&frac14;'),(9,'Half','&frac12;');
-/*!40000 ALTER TABLE `measurements` ENABLE KEYS */;
-UNLOCK TABLES;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `method_items`
 --
 
 DROP TABLE IF EXISTS `method_items`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `method_items` (
   `id` int(11) NOT NULL auto_increment,
-  `description` text,
+  `description` text NOT NULL,
   `position` int(11) default NULL,
-  `recipe_id` int(11) default NULL,
+  `recipe_id` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
-
---
--- Dumping data for table `method_items`
---
-
-LOCK TABLES `method_items` WRITE;
-/*!40000 ALTER TABLE `method_items` DISABLE KEYS */;
-/*!40000 ALTER TABLE `method_items` ENABLE KEYS */;
-UNLOCK TABLES;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `ratings`
 --
 
 DROP TABLE IF EXISTS `ratings`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `ratings` (
   `id` int(11) NOT NULL auto_increment,
   `recipe_id` int(11) default NULL,
   `value` int(11) default NULL,
   `user_id` int(11) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `ratings`
+-- Table structure for table `recipe_ingredients`
 --
 
-LOCK TABLES `ratings` WRITE;
-/*!40000 ALTER TABLE `ratings` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ratings` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `recipe_ingredients`;
+CREATE TABLE `recipe_ingredients` (
+  `recipe_id` int(11) NOT NULL,
+  `ingredient_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY  (`recipe_id`,`ingredient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `recipes`
 --
 
 DROP TABLE IF EXISTS `recipes`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `recipes` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
@@ -183,54 +119,35 @@ CREATE TABLE `recipes` (
   `difficulty` int(2) default '1',
   `freezable` int(1) default '0',
   `created` datetime default NULL,
-  `updated` datetime default NULL,
+  `updated` timestamp NULL default NULL,
   `ingredients_count` int(11) default '0',
-  `creator_id` int(11) default NULL,
+  `creator_id` int(11) NOT NULL,
   `comments_count` int(11) default '0',
   `ratings_count` int(11) default '0',
   `view_count` int(9) default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `recipes`
+-- Table structure for table `sessions`
 --
 
-LOCK TABLES `recipes` WRITE;
-/*!40000 ALTER TABLE `recipes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `recipes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `schema_info`
---
-
-DROP TABLE IF EXISTS `schema_info`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `schema_info` (
-  `version` int(11) default NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
-
---
--- Dumping data for table `schema_info`
---
-
-LOCK TABLES `schema_info` WRITE;
-/*!40000 ALTER TABLE `schema_info` DISABLE KEYS */;
-INSERT INTO `schema_info` VALUES (13);
-/*!40000 ALTER TABLE `schema_info` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
+  `id` varchar(32) collate utf8_unicode_ci NOT NULL default '',
+  `user_id` int(11) default NULL,
+  `expire` int(11) NOT NULL default '0',
+  `updated` int(11) NOT NULL default '0',
+  `data` text collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `foreign_username` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Contains web connection user sessions';
 
 --
 -- Table structure for table `taggings`
 --
 
 DROP TABLE IF EXISTS `taggings`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `taggings` (
   `id` int(11) NOT NULL auto_increment,
   `tag_id` int(11) NOT NULL,
@@ -238,54 +155,28 @@ CREATE TABLE `taggings` (
   `taggable_type` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `index_taggings_on_tag_id_and_taggable_id_and_taggable_type` (`tag_id`,`taggable_id`,`taggable_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
-
---
--- Dumping data for table `taggings`
---
-
-LOCK TABLES `taggings` WRITE;
-/*!40000 ALTER TABLE `taggings` DISABLE KEYS */;
-/*!40000 ALTER TABLE `taggings` ENABLE KEYS */;
-UNLOCK TABLES;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `tags`
 --
 
 DROP TABLE IF EXISTS `tags`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `tags` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `index_tags_on_name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
-
---
--- Dumping data for table `tags`
---
-
-LOCK TABLES `tags` WRITE;
-/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
-UNLOCK TABLES;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL auto_increment,
-  `password` varchar(50) NOT NULL default '',
-  `salt` varchar(255) NOT NULL default '',
-  `password_hash` varchar(255) NOT NULL default '',
+  `password` varchar(64) character set utf8 collate utf8_unicode_ci NOT NULL,
   `name` varchar(60) NOT NULL default '',
   `email` varchar(255) NOT NULL default '',
   `created` datetime NOT NULL,
@@ -294,9 +185,12 @@ CREATE TABLE `users` (
   `recipes_count` int(11) default '0',
   `comments_count` int(11) default '0',
   `ratings_count` int(11) default '0',
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -306,4 +200,3 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-06-17 16:24:36
