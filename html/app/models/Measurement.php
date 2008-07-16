139b23ace93207272ccec8415cc9345ef89ecd07
cookery <cookery@catharsis.co.uk>
2008-07-16 11:47:55 +0000

    Added in abbreviation to html/app/models/Measurement.php Commented out logging in html/app/controllers/RecipeController.php Switch html/app/controllers/IngredientController.php to use the new insert in html/app/model/Ingredient.php Added AJAX code for Measurements in html/app/controllers/AjaxController.php

--- html/app/models/Measurement.php
+++ html/app/models/Measurement.php
@@ -16,21 +16,46 @@ class Measurement extends Zend_Db_Table_Abstract {
 			'required' => false,
 			'label' => 'Measurement',
 			'validators' => array(
-				array( 'alnum', true, true )
+				array( 'alnum', true )
+			)
+		) ),
+		array( 'text', 'measurement_abbr', array(
+			'id' => 'measurement-abbr',
+			'required' => false,
+			'label' => 'Measurement Abbr',
+			'validators' => array(
+				array( 'alnum', true )
 			)
 		) )
+
 	);
 
-	// May be able to delete this
-	function __construct( $prefetch = true )
+	function __construct()
 	{
-		if ( ! $prefetch === false ) unset( $_rowClass );
 		$this->db = Zend_Registry::get("db");
 		Zend_Db_Table_Abstract::setDefaultAdapter($this->db);
 		
 		$this->log = Zend_Registry::get('log');
 		
 		$this->_setup();
+	}
+
+	/**
+	 * Override the insert to try and not throw an exception back if it fails
+	 * @param $data array associative array of vars to go in the db
+	 * @return $row Zend_Db_Table_Row the row of the Ingredient table
+	 */
+
+	function insert( $data )
+	{
+		try {
+			$parent::insert( $data );
+		} catch (Exception $e) {
+			// Doesnt matter if we cannot insert the ingredient
+			$this->log->info( 'Measurement ' . sq_brackets( $data['name'] ) . ' already exists' );
+		}
+
+		return $this->getByName( $data['name'] );
 	}
 
 }
