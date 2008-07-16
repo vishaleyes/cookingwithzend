832fa0c1bb3f9cb153152ef0806ede80df9f74b2
cookery@catharsis.co.uk <cookery@catharsis.co.uk@64c92516-bd51-0410-8049-8bbf4b82af0d>
2008-07-16 16:39:02 +0000

    Added in post/pre Dispatch, made comments as standard

--- trunk/html/app/controllers/RatingController.php
+++ trunk/html/app/controllers/RatingController.php
@@ -1,32 +1,40 @@
 <?php
 
 
-
-/*
-
-@author punky
-
-Handles the ratings system for recipe.
-
-*/
+/**
+ * Handles the ratings system for recipe.
+ * @author punky
+ */
 
 class RatingController extends DefaultController 
 {
 	
+	/**
+	 * This happens before the page is dispatch
+	 */
+
+	public function preDispatch() {
+		// $this->login();
+	}
+
 	public function indexAction() {
-		
-		
 	}
 	
 	public function addAction() {
-		
 		
 		$rating_message = "No rating or recipe specified";
 		
 		$this->view->pageContent = $this->pagesFolder.'/rating/add.phtml';
 		
 		echo $this->_response->setBody($this->view->render($this->templatesFolder."/home.tpl.php"));
-				
 		
+	}
+
+	/**
+	 * This happens after the page is dispatch
+	 */
+
+	public function postDispatch() {
+		exit;
 	}
 }
