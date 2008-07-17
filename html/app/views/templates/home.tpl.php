254659c156dd1908f642fb5227523f2e84999549
cookery <cookery@catharsis.co.uk>
2008-07-17 14:33:04 +0000

    Added navigation include

--- html/app/views/templates/home.tpl.php
+++ html/app/views/templates/home.tpl.php
@@ -1,4 +1,5 @@
 <?php echo $this->render( $this->includesFolder . '/header.php' ); ?>
+<?php echo $this->render( $this->includesFolder . '/navigation.php'); ?>
 <?php echo $this->render( $this->includesFolder . '/user_details.php'); ?>
 <?php echo $this->render( $this->pageContent ); ?>
 <?php echo $this->render( $this->includesFolder . '/footer.php' ); ?>
