8e366fdce3b825141778772862bcc5a2f2eb9163
cookery <cookery@catharsis.co.uk>
2008-07-25 09:57:10 +0000

    Changed nav and login

--- html/app/views/includes/navigation.php
+++ html/app/views/includes/navigation.php
@@ -1,6 +1,11 @@
 <!-- Navigation -->
 <ul>
-	<li>Home</li>
+	<li><a href="/">Home</a></li>
+<?php if ($this->session->user): ?>
+	<li><a href="<?php echo '/recipe/index/user_id/'.$this->session->user['id'] ?>">My Recipes</a></li>	
 	<li><a href="/recipe/new">New Recipe</a></li>
+<?php else: ?>
+	<li><a href="/user/login">Login</a></li>
+<?php endif ?>
 </ul>
 <!-- End Navigation -->
