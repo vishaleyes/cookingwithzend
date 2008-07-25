ca1a60755ff112667bc6b2d1e9b507ac846dd9c8
cookery <cookery@catharsis.co.uk>
2008-07-25 15:06:44 +0000

    Added Register to nav

--- html/app/views/includes/navigation.php
+++ html/app/views/includes/navigation.php
@@ -5,7 +5,7 @@
 	<li><a href="<?php echo '/recipe/index/user_id/'.$this->session->user['id'] ?>">My Recipes</a></li>	
 	<li><a href="/recipe/new">New Recipe</a></li>
 <?php else: ?>
-	<li><a href="/user/login">Login</a></li>
+	<li><a href="/user/login">Login</a> / <a href="/user/new">Register</a></li>
 <?php endif ?>
 </ul>
 <!-- End Navigation -->
