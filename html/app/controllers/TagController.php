39024164676682d9f96160d7d902f73607e02276
cookery <cookery@catharsis.co.uk>
2008-07-28 06:23:09 +0000

    Added routes for Tag and Tag deletion action

--- html/app/controllers/TagController.php
+++ html/app/controllers/TagController.php
@@ -3,17 +3,36 @@
 class TagController extends DefaultController  
 {
 
+	public function preDispatch()
+	{
+		// Held in DefaultController
+		$this->loggedIn( array( 'index' ) );
+	}
+
 	public function indexAction()
 	{
 		$tags = explode( ' ', $this->_getParam( 'name' ) );
 		$t = new Tag();
 		print_r( $t->getTagObjects( $tags ) );
+		
+		// Not sure what to do with this view just yet
+	}
+
+	/**
+	 * Delete a tag from the recipe
+	 */
+
+	public function deleteAction()
+	{
+		$this->db->delete( 'taggings', 'id = '.$this->_getParam('id') );
+		// Go back to where we came from
+		$this->_redirect( $_SERVER['HTTP_REFERER'] );
 	}
 
 	/**
 	 * We are existing after the action is dispatched
-	 *
 	 */
+	
 	public function postDispatch() {
 		exit;
 	}
