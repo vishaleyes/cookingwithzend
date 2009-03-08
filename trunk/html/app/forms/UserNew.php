844809e9703f05be9ed11c4267d77089fc27cc3a
cookery@catharsis.co.uk <cookery@catharsis.co.uk@64c92516-bd51-0410-8049-8bbf4b82af0d>
2009-03-08 22:02:34 +0000

    User creation back up and running

---
+++ trunk/html/app/forms/UserNew.php
@@ -0,0 +1,43 @@
+<?php
+
+class Forms_UserNew extends Zend_Form
+{
+
+	public function init()
+	{
+		// set the method for the display form to POST
+		$this->setMethod('post');
+
+		// add an email element
+		$this->addElement('text', 'email', array(
+			'label'      => 'E-mail:',
+			'required'   => true,
+			'filters'    => array('StringTrim'),
+			'validators' => array(
+				new Zend_Validate_NotEmpty(),
+				new Zend_Validate_EmailAddress()
+			)
+		))
+		->addElement('password', 'password', array(
+			'label'      => 'Password:',
+			'required'   => true,
+			'filters'    => array('StringTrim'),
+			'validators' => array(
+				new Zend_Validate_NotEmpty(),
+				new Zend_Validate_Alnum(),
+				new Zend_Validate_StringLength( array(3,255) )
+			)
+		))
+		->addElement('text', 'name', array(
+			'label'      => 'Name:',
+			'required'   => true,
+			'filters'    => array('StringTrim'),
+			'validators' => array(
+				new Zend_Validate_NotEmpty(),
+				new Zend_Validate_Alnum(),
+				new Zend_Validate_StringLength( array(3,255) )
+			)
+		))
+		->addElement('submit', 'submit');
+	}
+}
