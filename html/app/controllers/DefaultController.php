f2b5d78009b6b174f43226c00e58ed780ac16094
cookery <cookery@catharsis.co.uk>
2009-03-09 11:29:47 +0000

    Documentation changes

--- html/app/controllers/DefaultController.php
+++ html/app/controllers/DefaultController.php
@@ -1,68 +1,76 @@
 <?php
 
 /**
- * Default Controller - An Abstract class that extends Zend_Controller_Action and should be used as a parent class for all other controllers
+ * Default Controller - An Abstract class that extends Zend_Controller_Action
+ * and should be used as a parent class for all other major controllers
  */
 
 abstract class DefaultController extends Zend_Controller_Action {
 	
 	/**
 	* Application's Version number (for example 1.0.0)
-	*
 	* @var string
 	*/
 	protected $_release_no;
+
 	/**
-	* Environment (for example Production/Staging/Development)
-	*
-	* @var string
-	*/
+	 * Environment (for example Production/Staging/Development)
+	 * @var string
+	 */
 	protected $_environment;
+
 	/**
-	* Config object - based on config.xml file
-	*
-	* @var Zend_Config
-	*/
+	 * Config object - based on config.xml file
+	 * @var Zend_Config
+	 */
 	protected $_config;
+
 	/**
-	* Database object (for example Zend_Db_Adapter_Pdo_Mysql object)
-	*
-	* @var object
-	*/
+	 * Database object (for example Zend_Db_Adapter_Pdo_Mysql object)
+	 * @var object
+	 */
 	protected $_db;
+
 	/**
-	* Logger Object (usage : $this->log->log("Message to log", 0);)
-	*
-	* @var Zend_Log
-	*/
+	 * Logger Object (usage : $this->log->log("Message to log", 0);)
+	 * @var Zend_Log
+	 */
 	protected $_log;
+
 	/**
 	 * Session object (usage : $this->session->session_key = $session_value)
 	 * @var Zend_Session
 	 */
 	protected $_session;
+	
 	/**
-	* Cache Object
-	*
-	* @var Zend_Cache
-	*/
+	 * Cache Object
+	 * @var Zend_Cache
+	 */
 	protected $_cache;
+	
 	/**
 	 * Folder where all pages should be stored
 	 * @var string
 	 */
 	protected $_pagesFolder;
+	
 	/**
 	 * Folder where all temlpates should be stored
 	 * @var string
 	 */
 	protected $_templatesFolder;
+	
 	/**
 	 * Folder where all other application's specific include files should be stored
 	 * @var string
 	 */
 	protected $_includesFolder;
 	
+	/**
+	 * Holder for the flash messenger that is used to tell users whats happening
+	 * @var Zend_Controller_Action_Helper_FlashMessenger
+	 */
 	protected $_flashMessenger;
 	
 	public $model;
